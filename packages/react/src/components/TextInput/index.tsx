d865fc3f76dd5acffe156975a6b1f4711b99d0dd
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2023-03-06 18:08:34 -0300

    feat: base do projeto

---
+++ packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,20 @@
+import { ComponentProps, forwardRef, ElementRef } from 'react';
+
+import * as S from './styles';
+
+export interface TextInputProps extends ComponentProps<typeof S.Input> {
+	prefix?: string;
+}
+
+export const TextInput = forwardRef<ElementRef<typeof S.Input>, TextInputProps>(
+	({ prefix, ...props }: TextInputProps, ref) => {
+		return (
+			<S.TextInputContainer>
+				{!!prefix && <S.Prefix>{prefix}</S.Prefix>}
+				<S.Input ref={ref} {...props} />
+			</S.TextInputContainer>
+		);
+	}
+);
+
+TextInput.displayName = 'TextInput';
